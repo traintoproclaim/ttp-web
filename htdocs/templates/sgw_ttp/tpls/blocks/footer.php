7c5f12474e84e226331ccdbbcf40ab8d8651126a
cambell-prince <cambell.prince@gmail.com>
2014-02-26 15:50:26 +1300

    Footer

--- htdocs/templates/sgw_ttp/tpls/blocks/footer.php
+++ htdocs/templates/sgw_ttp/tpls/blocks/footer.php
@@ -11,13 +11,26 @@ defined('_JEXEC') or die;
 <!-- FOOTER -->
 <footer id="t3-footer" class="wrap t3-footer">
 
-	<?php if ($this->checkSpotlight('footnav', 'footer-1, footer-2, footer-3, footer-4, footer-5, footer-6')) : ?>
-		<!-- FOOT NAVIGATION -->
-		<div class="container hidden-xs">
-			<?php $this->spotlight('footnav', 'footer-1, footer-2, footer-3, footer-4, footer-5, footer-6') ?>
+	<!-- FOOT NAVIGATION -->
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 footer-info">
+				<?php if ($this->countModules('footer-info')) : ?>
+				<jdoc:include type="modules" name="<?php $this->_p('footer-info') ?>" style="T3xhtml" />
+				<?php endif ?>
+			</div>
+			<div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 footer-links">
+				<?php if ($this->countModules('footer-subcribe')) : ?>
+				<jdoc:include type="modules" name="<?php $this->_p('footer-subcribe') ?>" style="T3xhtml" />
+				<?php endif ?>
+
+				<?php if ($this->checkSpotlight('footnav', 'footer-1, footer-2, footer-3, footer-4, footer-5, footer-6')) : ?>
+				<?php $this->spotlight('footnav', 'footer-1, footer-2, footer-3, footer-4, footer-5, footer-6') ?>
+				<?php endif ?>
+			</div>
 		</div>
-		<!-- //FOOT NAVIGATION -->
-	<?php endif ?>
+	</div>
+	<!-- //FOOT NAVIGATION -->
 
 	<section class="t3-copyright">
 		<div class="container">
@@ -28,12 +41,12 @@ defined('_JEXEC') or die;
             <a href="http://twitter.github.io/bootstrap/" target="_blank">Bootstrap</a> is a front-end framework of Twitter, Inc. Code licensed under <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">Apache License v2.0</a>.
           </small>
           <small>
-            <a href="http://fortawesome.github.io/Font-Awesome/" target="_blank">Font Awesome</a> font licensed under <a href="http://scripts.sil.org/OFL">SIL OFL 1.1</a>.
+            <a href="http://fontawesome.github.io/Font-Awesome/" target="_blank">Font Awesome</a> font licensed under <a href="http://scripts.sil.org/OFL">SIL OFL 1.1</a>.
           </small>
 				</div>
 				<?php if ($this->getParam('t3-rmvlogo', 1)): ?>
 					<div class="col-md-4 poweredby text-hide">
-						<a class="t3-logo t3-logo-color" href="http://t3-framework.org" title="<?php echo JText::_('T3_POWER_BY_TEXT') ?>"
+						<a class="t3-logo t3-logo-light" href="http://t3-framework.org" title="<?php echo JText::_('T3_POWER_BY_TEXT') ?>"
 						   target="_blank" <?php echo method_exists('T3', 'isHome') && T3::isHome() ? '' : 'rel="nofollow"' ?>><?php echo JText::_('T3_POWER_BY_HTML') ?></a>
 					</div>
 				<?php endif; ?>
